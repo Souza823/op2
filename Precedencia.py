5c2252d825206a2f7060f5509861eaab9e9476ce
Souza823 <treinamentosgis@gmail.com>
2025-04-26 17:38:51 -0300

    Exemplos de Precedência de Operações

---
+++ Precedencia.py
@@ -0,0 +1,22 @@
+x=10-5*2
+print(x)
+#na matemática será igual a zero, o pythpn também segue essa ordem
+#Na ordem
+#Parentesis
+#Expoentes
+#Multiplicações e divisões (da esquerda para direita)
+#Somas e subtrações
+print((10-5)*2)
+print(10**2*2)
+print(10**2**2)
+print(10**(2*2))
+print(10/2*4)
+
+produto_1= 10
+produto_2= 20
+print(produto_1+produto_2)
+print(produto_1-produto_2)
+print(produto_1/produto_2)
+print(produto_1//produto_2)
+print(produto_1*produto_2)
+print(produto_1 % produto_2)
